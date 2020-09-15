027e4255d66e5553af8d58aafa7cd1da4a3f3ec2
zeref80 <45498824+zeref80@users.noreply.github.com>
2020-09-15 19:00:50 +0200

    2D, 3D Sterowanie, Wieżyczka

---
+++ Tank2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _horizontalInput = 0;
+    private float _verticalInput = 0;
+    private Vector3 _mousePosition;
+    PlayerTankMovement movementScript;
+
+    void Start()
+    {
+        movementScript = GetComponent<PlayerTankMovement>();
+    }
+
+    void FixedUpdate()
+    {
+        movementScript.MovePlayer(Mathf.Abs(_verticalInput));
+        if (_horizontalInput != 0)
+            movementScript.RotateTankBody(_horizontalInput);  
+
+        //Vertical w jedną stronę, do przyjrzenia się potem
+
+    }
+
+    void Update()
+    {
+        GetPlayerInput();
+        movementScript.RotateTankTurret(_mousePosition);
+    }
+
+    private void GetPlayerInput()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");   
+        Vector3 mousePosition3d = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        _mousePosition = new Vector3(mousePosition3d.x, mousePosition3d.y, 0);
+
+    }
+}
---
+++ Tank2D/Assets/Scripts/PlayerTankMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTankMovement : MonoBehaviour
+{
+    public int movementSpeed = 0;
+    public int rotationSpeedTank = 0;
+    public int rotationSpeedTurret = 0;
+
+    Transform tankBody;
+    Transform tankTurret;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        tankBody = transform.Find("Chassie");
+        tankTurret = transform.Find("Turret");
+
+    }
+
+    public void MovePlayer(float inputValue)
+    {
+        rb2d.velocity = tankBody.right * inputValue * movementSpeed;
+    }
+
+
+    public void RotateTankTurret(Vector3 endpoint)
+    {
+        Quaternion desiredRotation = Quaternion.LookRotation(Vector3.forward, endpoint - tankTurret.position);
+        desiredRotation = Quaternion.Euler(0, 0, desiredRotation.eulerAngles.z + 900);
+        tankTurret.rotation = Quaternion.RotateTowards(tankTurret.rotation, desiredRotation, rotationSpeedTurret * Time.deltaTime);
+
+    }
+
+
+    public void RotateTankBody(float inputDirection)
+    {
+        float rotation = -inputDirection * rotationSpeedTank;
+        tankBody.Rotate(Vector3.forward * rotation);
+
+    }
+}
